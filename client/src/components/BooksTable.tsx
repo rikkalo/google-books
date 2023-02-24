f62759fc3f49d84ed9deabfeed7229dd822fd723
Anastasiia Lopatina <stasialopatina@gmail.com>
2023-02-24 06:17:44 +0100

    chore: add config for lint

--- client/src/components/BooksTable.tsx
+++ client/src/components/BooksTable.tsx
@@ -1,15 +1,15 @@
-import { Image, Link, Button } from "@chakra-ui/react";
-import { Table } from "react-chakra-pagination";
-import type { Book } from "@redkubes-homework/model";
-import type { CellContext } from "@tanstack/table-core";
+import { Image, Link, Button } from '@chakra-ui/react'
+import { Table } from 'react-chakra-pagination'
+import type { Book } from '@redkubes-homework/model'
+import type { CellContext } from '@tanstack/table-core'
 
 export type BooksTableProps = {
-  books: Book[];
-  page: number;
-  itemsPerPage: number;
-  totalRegisters: number;
-  setPage: (page: number) => void;
-};
+  books: Book[]
+  page: number
+  itemsPerPage: number
+  totalRegisters: number
+  setPage: (page: number) => void
+}
 
 export function BooksTable({
   books,
@@ -18,62 +18,62 @@ export function BooksTable({
   itemsPerPage,
   totalRegisters,
 }: BooksTableProps) {
-  // Just because of the nice library choice ^_^
+  // TODO This code fixes bug with react-chakra-pagination
   const tableData = Array.from({ length: (page - 1) * itemsPerPage })
     .fill({})
     .concat(
       books.map(({ volumeInfo, saleInfo }: Book) => ({
         thumbnail: volumeInfo.imageLinks?.smallThumbnail,
         title: volumeInfo.title,
-        author: volumeInfo.authors?.join(", "),
+        author: volumeInfo.authors?.join(', '),
         language: volumeInfo.language,
         buyLink: saleInfo.buyLink,
-      }))
-    );
+      })),
+    )
 
   const tableColumns = [
     {
-      Header: "Thumbnail",
-      accessorKey: "thumbnail" as const,
+      Header: 'Thumbnail',
+      accessorKey: 'thumbnail' as const,
       cell: (props: CellContext<any, string>) => (
         <Image src={props.cell.getValue()} alt={props.cell.getValue()} />
       ),
     },
     {
-      Header: "Title",
-      accessorKey: "title" as const,
+      Header: 'Title',
+      accessorKey: 'title' as const,
     },
     {
-      Header: "Author",
-      accessorKey: "author" as const,
+      Header: 'Author',
+      accessorKey: 'author' as const,
     },
     {
-      Header: "Language",
-      accessorKey: "language" as const,
+      Header: 'Language',
+      accessorKey: 'language' as const,
     },
     {
-      Header: "Link",
-      accessorKey: "buyLink" as const,
+      Header: 'Link',
+      accessorKey: 'buyLink' as const,
       cell: (props: CellContext<any, string>) => (
         <>
           {props.cell.getValue() ? (
-            <Button colorScheme="purple">
+            <Button colorScheme='purple'>
               <Link href={props.cell.getValue()} isExternal>
                 Buy
               </Link>
             </Button>
           ) : (
-            "No link"
+            'No link'
           )}
         </>
       ),
     },
-  ];
+  ]
   return (
     <>
       <Table
-        colorScheme="purple"
-        emptyData={{ text: "No books" }}
+        colorScheme='purple'
+        emptyData={{ text: 'No books' }}
         totalRegisters={totalRegisters}
         itemsPerPage={itemsPerPage}
         page={page}
@@ -82,5 +82,5 @@ export function BooksTable({
         data={tableData}
       />
     </>
-  );
+  )
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import EnvironmentPlugin from "vite-plugin-environment";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import EnvironmentPlugin from 'vite-plugin-environment'
 
-const PORT = Number(process.env.PORT ?? 5137);
-const API_URL = process.env.API_URL ?? "https://localhost:3000";
+const PORT = Number(process.env.PORT ?? 5137)
+const API_URL = process.env.API_URL ?? 'http://localhost:3000'
 
 export default defineConfig({
   server: {
@@ -12,4 +12,4 @@ export default defineConfig({
     strictPort: true,
   },
   plugins: [react(), EnvironmentPlugin({ API_URL })],
-});
+})
